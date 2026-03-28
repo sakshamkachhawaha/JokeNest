79d99b7393045bd19b5972e508aa5668d4888ac8
sakshamkachhawaha <sakshamkachhawaha2007@gmail.com>
2026-03-28 19:07:49 +0530

    Landing Page Ui Done

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,21 @@
+const welcomeSection = document.querySelector(".welcome-section");
+const jokeSection = document.querySelector(".joke-section");
+const moodCards = document.querySelectorAll(".mood-card");
+const backBtn = document.querySelector(".back-btn");
+
+moodCards.forEach(card => {
+  card.addEventListener("click", () => {
+    welcomeSection.classList.add("hidden");
+    jokeSection.classList.remove("hidden");
+  });
+});
+
+backBtn.addEventListener("click", () => {
+  jokeSection.classList.add("hidden");
+  welcomeSection.classList.remove("hidden");
+});
+
 fetch("https://v2.jokeapi.dev/joke/Any")
   .then(res => res.json())
   .then(data => console.log(data));
+  
